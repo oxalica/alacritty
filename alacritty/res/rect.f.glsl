9cace2ef253f14351ab925241f6be739c74392e8
oxalica <oxalicc@pm.me>
2021-09-11 01:16:06 +0800

    Render dotted and dashed underline

--- alacritty/res/rect.f.glsl
+++ alacritty/res/rect.f.glsl
@@ -1,10 +1,17 @@
 #version 330 core
 
 flat in vec4 color;
+flat in float dashPeriod;
+flat in vec2 startPos;
+in vec2 relativePos;
 
 out vec4 FragColor;
 
 void main()
 {
+    float dist = (relativePos.x - startPos.x) / dashPeriod;
+    if (fract(dist) > 0.5)
+        discard;
+
     FragColor = color;
 }
--- alacritty/res/rect.v.glsl
+++ alacritty/res/rect.v.glsl
@@ -1,11 +1,18 @@
 #version 330 core
 layout (location = 0) in vec2 aPos;
 layout (location = 1) in vec4 aColor;
+layout (location = 2) in float aDashPeriod;
 
 flat out vec4 color;
+flat out float dashPeriod;
+flat out vec2 startPos;
+out vec2 relativePos;
 
 void main()
 {
     color = aColor;
-    gl_Position = vec4(aPos.x, aPos.y, 0.0, 1.0);
+    dashPeriod = aDashPeriod;
+    startPos = aPos.xy;
+    relativePos = aPos.xy;
+    gl_Position = vec4(aPos.xy, 0.0, 1.0);
 }
